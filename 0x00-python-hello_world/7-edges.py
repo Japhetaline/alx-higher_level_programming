f18ebf512473f1be4b5a58c247292ebaa5af2eb3
Japhetaline <Japhetdamassoh@gmail.com>
2023-05-08 20:16:01 -0700

    7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_3_word = word[:3]
+last_two_word = word[-2:]
+middle_word = word[1:-1]
+print("first 3 letters: {}".format(first_3_word))
+print("Last 2 letters: {}".format(last_two_word))
+print("Middle word: {}".format(middle_word))
