0804f058d54be276d05e3c573bbce5ba5586291d
Japhetaline <Japhetdamassoh@gmail.com>
2023-05-09 22:21:27 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-1000,1000)
-japNum = number
+japNum = abs(number) % 10
 
 if number < 0:
-    number = -(number)
-
-    lastDigit = number % 10
-    if japNum < 0:
-        number = japNum
-        lastDigit = -(lastDigit)
-
-        if lastDigit > 5:
-            thread = "and is greather than 5"
-        elif lastDigit == 0:
-            thread = "and is 0"
-        elif lastDigit < 6:
-            thread = "and is less than 6 and not 0"
-
-            print("Last digit of {:d} is {:d}".format(number, lastDigit), thread)
+    japNum = -(japNum)
+    print("Last digit of {} is {} and is".format(number, japNum), end="")
+    if japNum > 5:
+        print("greather than 5")
+    elif japNum == 0:
+        print("0")
+    else:
+        print("less than 6 and not 0")
