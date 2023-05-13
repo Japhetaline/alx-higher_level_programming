8f08fc4420a9ab3d5d89b9f37acc51e49340c7d6
Japhetaline <Japhetdamassoh@gmail.com>
2023-05-13 12:47:02 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/pythons
+#0-print_list_integer.py
 
 def print_list_integer(my_list=[]):
     """
     printing a lists of integers
     """
 
-    for integer in my_list:
-        print("{:d}".format(integer))
+    for k in my_list:
+        print("{:d}".format(k))
