50478d917f024931af4349bf46c498e716171570
Japhetaline <Japhetdamassoh@gmail.com>
2023-05-13 13:41:57 -0700

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def elemen_at(my_list, idx):
+    """
+    Gets an element in a list at given index and return it
+    """
+    if idx >= len(my_list) or idx < 0
+    return
+
+return (my_list[idx])
