13a988e02a97ac1fa13c721d612317e16f36ee2a
Japhetaline <Japhetdamassoh@gmail.com>
2023-06-05 13:44:22 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Empty class that define Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Empty clas Rectangle that defines a rectangle figure
+
+    Parameters:
+    empty
+    """
+
+    pass
