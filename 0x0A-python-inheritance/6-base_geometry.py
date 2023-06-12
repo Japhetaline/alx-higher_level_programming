2a679933a21e1a48c06d9a9783cb128c4f151cf5
Japhetaline <Japhetdamassoh@gmail.com>
2023-06-12 14:25:41 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+@author: Damassoh Japhet
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
+
+    def area(self):
+        """
+        Public instance method that calculates the area
+
+        Raises:
+            Exception if area is not imlemented
+        """
+        raise Exception("area() is not implemented")
