d12afa2cec8695221d8c39c96c726e433e3050cb
Japhetaline <Japhetdamassoh@gmail.com>
2023-06-13 06:20:08 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     Read a file and print stdout
     """
     with open(filename, encoding='utf-8') as file:
-        print(file.read, end='')
+        print(file.read(), end='')
