c1fffec8941cc3b2e5513b884a28ac3930c3c78d
Japhetaline <Japhetdamassoh@gmail.com>
2023-06-13 07:21:11 -0700

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+@author: Damassoh Japhet
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Function that convert strings to JSON
+    """
+    return json.loads(my_str)
