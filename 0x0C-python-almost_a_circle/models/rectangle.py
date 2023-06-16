b0f8a8d746acad4be2fd555dbc1221692589fa64
Japhetaline <Japhetdamassoh@gmail.com>
2023-06-16 10:56:28 -0700

    add_area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,123 +2,131 @@
 """
 @author: Damassoh Japhet
 """
-
 from models.base import Base
 
 
 class Rectangle(Base):
     """
-    class Rectangle
+    Class Rectangle
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Class Constructor for Rectangle
 
         Attributes:
-        width (int): Private attribute for the width of the Rectangle
-        height (int): Private attribute for the height of the Rectangle
-        x (int): Private attribute for x value of the Rectangle
-        y (int): Private attribute for y value of the Rectangle
-        id (int): Private attribute id inherits from Base
+            width (int): Private attribute for the width of the Rectangle
+            height (int): Private attribute for the height of the Rectangle
+            x (int): Private attribute for x value of the Rectangle
+            y (int): Private attribute for y value of the Rectangle
+            id (int): Private attribute id inherits from Base
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-        @property
-        def width(self):
-            """
-            property method for width value
+    @property
+    def width(self):
+        """
+        Property method for width value
 
-            Return:
+        Return:
             Private value width value
-            """
-            return self.__width
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """
-            setter method for width
+    @width.setter
+    def width(self, value):
+        """
+        setter method for width value
 
-            Attribute:
-            value (int): value to check if is int and greater than 0
-            """
-            if type(value) != int:
-                raise TypeError("width must be an integer")
-            elif value <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = value
+        Attribute:
+            value (int): value to check if is int and gratter than 0
+        """
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
 
-        @property
-        def height(self):
-            """
-            property method for height
+    @property
+    def height(self):
+        """
+        Property method for height value
 
-            Return:
-            private value height value
-            """
-            return self.__height
+        Return:
+            Private value height value
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """
-            setter method for height
+    @height.setter
+    def height(self, value):
+        """
+        setter method for height value
 
-            Attribute:
-            value (int): value to check if is int and greater than 0
-            """
-            if type(value) != int:
-                raise TypeError("height must be an integer")
-            elif value <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
+        Attribute:
+            value (int): value to check if is int and gratter than 0
+        """
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        elif value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
 
-        @property
-        def x(self):
-            """
-            property method for x
+    @property
+    def x(self):
+        """
+        Property method for x value
 
-            Return:
+        Return:
             Private value x value
-            """
-            return self.__x
+        """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """
-            setter method for x
+    @x.setter
+    def x(self, value):
+        """
+        setter method for x value
 
-            Attribute:
-            value (int): value to check if is int and greater than 0
-            """
-            if type(value) != int:
-                raise TypeError("x must be an integer")
-            elif value < 0:
-                raise ValueError("x must be >= 0")
-            self.__x = value
+        Attribute:
+            value (int): value to check if is int and gratter than 0
+        """
+        if type(value) != int:
+            raise TypeError('x must be an integer')
+        elif value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
 
-        @property
-        def y(self):
-            """
-            property method for y
+    @property
+    def y(self):
+        """
+        Property method for y value
 
-            return
+        Return:
             Private value y value
-            """
-            return self.__y
+        """
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """
-            setter method for y
+    @y.setter
+    def y(self, value):
+        """
+        setter method for y value
 
-            Attribute:
-            value (int): value to check if is int and greater than 0
-            """
-            if type(value) != int:
-                raise TypeError("y must be an integer")
-            elif value < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = value
+        Attribute:
+            value (int): value to check if is int and gratter than 0
+        """
+        if type(value) != int:
+            raise TypeError('y must be an integer')
+        elif value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
+
+    def area(self):
+        """
+        Calculates the area of the rectangle
+
+        Returns:
+            The area of the rectangle
+        """
+        return self.width * self.height
