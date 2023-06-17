582c36499bebba1ac3b1d913c95a53670a6646da
Japhetaline <Japhetdamassoh@gmail.com>
2023-06-17 13:12:54 -0700

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@
 
 import json
 
+
 class Base:
     """
     Base class
@@ -21,3 +22,39 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Convert a list of dictionaries to a JSON string representation
+
+        Args:
+        list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+        str: The JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of a list instances
+        to a file.
+
+        Args:
+        list_objs (list): List of instances.
+
+        Returns:
+        None
+        """
+        filename = cls.__name__ + ".json"
+        string = []
+        if list_objs is not None:
+            for k in list_objs:
+                string.append(cls.to_dictionary(k))
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.write(cls.to_json_string(string))
