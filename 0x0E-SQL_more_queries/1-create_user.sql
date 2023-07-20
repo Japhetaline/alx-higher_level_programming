b6d55153599852afbb5022481351c57cdd225d31
Japhetaline <Japhetdamassoh@gmail.com>
2023-07-20 01:02:22 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
--- DDL query to create a  MyQSL user with password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
--- add a privileges to user
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- DDL query to create a MySQL user with pasword
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- add privileges to user
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
