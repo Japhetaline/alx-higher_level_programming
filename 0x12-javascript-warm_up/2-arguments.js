f1c8f530e6b9cc53d08c34b9e9b92af671ce5613
Japhetaline <Japhetdamassoh@gmail.com>
2023-08-14 07:51:34 -0700

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 const argumentc = process.argv.length;
-
 if (argumentc > 3) {
-console.log('Arguments found');
+  console.log('Arguments found');
 } else if (argumentc === 3) {
-console.log('Argument found');
+  console.log('Argument found');
 } else {
-console.log('No argument')
+  console.log('No argument');
 }
