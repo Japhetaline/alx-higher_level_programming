dd1b72085c6ddbf1ee00315e84ae7dda1e998c2f
Japhetaline <Japhetdamassoh@gmail.com>
2023-08-31 14:03:23 -0700

    curl

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Get request to the URL and display the body of the response
-curl-sLX PUT -d "user_id=98" -H "Origin: You got me!" 0.0.0.0:5000/catch_me
+
+response=$(curl -s -X PUT -d "user_id=98" "0.0.0.0:5000/catch_me")
+
+response_body=$(grep -oP '(?<=<body>).*(?=</body>)' <<< "$response")
+
+printf "%s\n" "$response_body"
