e9c7cf49654dd7f3410f9139dbc4d495c3f7cd25
Japhetaline <Japhetdamassoh@gmail.com>
2023-09-26 07:53:50 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,11 +3,9 @@ const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf8', function (error, data) {
-	if (error) {
-		console.log(error);
-	} else {
-		process.stdout.write(data);
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    process.stdout.write(data);
+  }
 });
-
-
